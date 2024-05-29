ed970bbfbc1f72c9079d82d16ed1e9623461fe71
eddieoz <eddieoz@pm.me>
2024-05-30 00:59:15 +0300

    added sharding for temporary global variables

--- shitcoin-horse-racing/command.js
+++ shitcoin-horse-racing/command.js
@@ -54,7 +54,7 @@ Brown: {{kicklet.GetCounter "oddsBrown"}}
 {{script.Call "main"}}
 {{$result := script.Var "result"}}
 
-{{else if eq $result "NOT_ENOUGH_POINTS"}}
+{{if eq $result "NOT_ENOUGH_POINTS"}}
     {{template "notEnoughPoints"}}
 {{else if eq $result "SYNTAX_ERROR"}}
     {{template "betSyntaxError"}}
@@ -96,16 +96,16 @@ Brown: {{kicklet.GetCounter "oddsBrown"}}
 
     async function main() {
         let bettingOpen = Kicklet.getTempGlobalVar('bettingOpen');
-        let bets = Kicklet.getTempGlobalVar('bets') || [];
         let totalBetPoints = Kicklet.getTempGlobalVar('totalBetPoints') || 0;
-
-        console.log('Current Bets:', bets.length);
 
         if (args.length == 1 && args[0] === 'open') {
             if (isAdmin()) {
                 Kicklet.setTempGlobalVar('bettingOpen', true, 600);
                 Kicklet.setTempGlobalVar('winningOption', '', 600);
                 Kicklet.setTempGlobalVar('totalBetPoints', 0, 600);
+                for (let i = 0; i < 4; i++) {
+                    Kicklet.setTempGlobalVar(`betsShard${i}`, '', 600);
+                }
                 Kicklet.setCounter('oddsWhite', 0);
                 Kicklet.setCounter('oddsBlue', 0);
                 Kicklet.setCounter('oddsGreen', 0);
@@ -129,7 +129,7 @@ Brown: {{kicklet.GetCounter "oddsBrown"}}
                 const winningOption = args[1];
                 if (options.includes(winningOption)) {
                     Kicklet.setTempGlobalVar('winningOption', winningOption, 600);
-                    await distributePoints(winningOption, bets, totalBetPoints);
+                    await distributePoints(winningOption, await getAllBets(), totalBetPoints);
                     result = 'WINNER_SET';
                 } else {
                     result = 'SYNTAX_ERROR';
@@ -139,7 +139,10 @@ Brown: {{kicklet.GetCounter "oddsBrown"}}
             }
         } else if (args.length == 2 && isInt(args[0]) && options.includes(args[1])) {
             if (bettingOpen) {
-                const existingBet = bets.find(bet => bet.username === username);
+                const bets = await getAllBets();
+                console.log(bets.length, $event.getSender());
+                {{/* const existingBet = bets.find(bet => bet.split(':')[0] === username); */}}
+                const existingBet = false;
                 if (existingBet) {
                     result = 'BET_ALREADY_PLACED';
                 } else {
@@ -149,15 +152,15 @@ Brown: {{kicklet.GetCounter "oddsBrown"}}
                     const userPoints = pointsResponse.data.points;
 
                     if (userPoints >= points) {
-                        bets.push({ username, points, option });
+                        const newBet = `${username}:${points}:${option}`;
+                        await addBet(newBet);
                         totalBetPoints += points;
 
-                        Kicklet.setTempGlobalVar('bets', bets, 600);
                         Kicklet.setTempGlobalVar('totalBetPoints', totalBetPoints, 600);
                         Kicklet.setCounter('totalBetPoints', totalBetPoints);
                         Kicklet.setPoints(username, userPoints - points);
-                        await calculateOdds(bets, totalBetPoints);
-                        
+                        await calculateOdds(await getAllBets(), totalBetPoints);
+
                         result = 'BET_ACCEPTED';
                     } else {
                         result = 'NOT_ENOUGH_POINTS';
@@ -168,40 +171,52 @@ Brown: {{kicklet.GetCounter "oddsBrown"}}
             }
         } else if (args.length == 1 && args[0] === 'odds') {
             result = 'SHOW_ODDS';
-            await calculateOdds(bets, totalBetPoints);
+            await calculateOdds(await getAllBets(), totalBetPoints);
         } else {
             result = 'SYNTAX_ERROR';
         }
     }
 
     async function distributePoints(winningOption, bets, totalBetPoints) {
-        const winningBets = bets.filter(bet => bet.option === winningOption);
-        const totalWinningPoints = winningBets.reduce((acc, bet) => acc + bet.points, 0);
+        const winningBets = bets.filter(bet => {
+            const [ , , option] = bet.split(':');
+            return option === winningOption;
+        });
+        const totalWinningPoints = winningBets.reduce((acc, bet) => {
+            const [ , points ] = bet.split(':');
+            return acc + parseInt(points, 10);
+        }, 0);
 
         for (const bet of winningBets) {
-            const pointsResponse = await Kicklet.getPoints(bet.username);
+            const [username, points] = bet.split(':');
+            const pointsResponse = await Kicklet.getPoints(username);
             const userPoints = pointsResponse.data.points;
-            const winShare = (bet.points / totalWinningPoints) * totalBetPoints;
+            const winShare = (parseInt(points, 10) / totalWinningPoints) * totalBetPoints;
             const winPoints = Math.floor(winShare);
-            
-            await Kicklet.setPoints(bet.username, userPoints + winPoints);
+
+            await Kicklet.setPoints(username, userPoints + winPoints);
         }
-        await resetPoll();
+        await clearBets();
     }
 
     async function calculateOdds(bets, totalBetPoints) {
-        const pointsOnOption = (option) => bets.filter(bet => bet.option === option).reduce((acc, bet) => acc + bet.points, 0);
+        const pointsOnOption = (option) => bets.filter(bet => {
+            const [ , , betOption] = bet.split(':');
+            return betOption === option;
+        }).reduce((acc, bet) => {
+            const [ , points ] = bet.split(':');
+            return acc + parseInt(points, 10);
+        }, 0);
 
         const oddsWhite = (pointsOnOption('white') / totalBetPoints).toFixed(2);
         const oddsBlue = (pointsOnOption('blue') / totalBetPoints).toFixed(2);
         const oddsGreen = (pointsOnOption('green') / totalBetPoints).toFixed(2);
         const oddsBrown = (pointsOnOption('brown') / totalBetPoints).toFixed(2);
 
-        Kicklet.setCounter('oddsWhite', oddsWhite*100);
-        Kicklet.setCounter('oddsBlue', oddsBlue*100);
-        Kicklet.setCounter('oddsGreen', oddsGreen*100);
-        Kicklet.setCounter('oddsBrown', oddsBrown*100);
-
+        Kicklet.setCounter('oddsWhite', oddsWhite * 100);
+        Kicklet.setCounter('oddsBlue', oddsBlue * 100);
+        Kicklet.setCounter('oddsGreen', oddsGreen * 100);
+        Kicklet.setCounter('oddsBrown', oddsBrown * 100);
     }
 
     async function resetPoll() {
@@ -213,6 +228,45 @@ Brown: {{kicklet.GetCounter "oddsBrown"}}
         Kicklet.setCounter('oddsGreen', 0);
         Kicklet.setCounter('oddsBrown', 0);
         Kicklet.setCounter('totalBetPoints', 0);
-        totalBetPoints = 0;
     }
+
+    async function getAllBets() {
+        let bets = [];
+        for (let i = 0; i < 4; i++) {
+            const shard = Kicklet.getTempGlobalVar(`betsShard${i}`) || "";
+            if (shard) {
+                bets = bets.concat(shard.split(';').filter(Boolean));
+            }
+        }
+        return bets;
+    }
+
+    async function addBet(newBet) {
+        for (let i = 0; i < 4; i++) {
+            let shard = Kicklet.getTempGlobalVar(`betsShard${i}`) || "";
+            const shardArray = shard.split(';').filter(Boolean);
+
+            if (shardArray.length < 25) {
+                shardArray.push(newBet);
+                Kicklet.setTempGlobalVar(`betsShard${i}`, shardArray.join(';'), 600);
+                return;
+            }
+        }
+        throw new Error("No space available to add new bet");
+    }
+
+    async function clearBets() {
+        for (let i = 0; i < 4; i++) {
+            Kicklet.deleteTempGlobalVar(`betsShard${i}`);
+        }
+        Kicklet.deleteTempGlobalVar('bettingOpen');
+        Kicklet.deleteTempGlobalVar('totalBetPoints');
+        Kicklet.setCounter('oddsWhite', 0);
+        Kicklet.setCounter('oddsBlue', 0);
+        Kicklet.setCounter('oddsGreen', 0);
+        Kicklet.setCounter('oddsBrown', 0);
+        Kicklet.setCounter('totalBetPoints', 0);
+    }
+
+
 </script>
